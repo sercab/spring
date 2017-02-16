ce5955a746e2d4644e05ece10f2f773826f64e6d
Eduardo Serrano <it.death.taxes@gmail.com>
2017-02-16 14:34:30 -0600

    Set Profile development

--- report/src/main/java/com/aurora/config/MvcWebApplicationInitializer.java
+++ report/src/main/java/com/aurora/config/MvcWebApplicationInitializer.java
@@ -1,9 +1,22 @@
 package com.aurora.config;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
 
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		
+		servletContext.setInitParameter("spring.profiles.active", "development");
+
+		//Set multiple active profile
+		//servletContext.setInitParameter("spring.profiles.active", "dev, testdb");
+	}
+	
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		// TODO Auto-generated method stub
